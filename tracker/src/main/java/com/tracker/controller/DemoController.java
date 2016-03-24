e9a9b022cf5dbb1a786682cb0d619d79748d0d8c
raj <tarange.raj@gmail.com>
2016-03-24 19:03:59 +0530

    - Logger Added

--- tracker/src/main/java/com/tracker/controller/DemoController.java
+++ tracker/src/main/java/com/tracker/controller/DemoController.java
@@ -1,15 +1,19 @@
 package com.tracker.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController 
 {
+	private static final Logger logger = Logger.getLogger(DemoController.class);
+	
 	@RequestMapping(value="/demo")
 	public String getScreen()
 	{
 		System.out.println("in demoo");
+		logger.info("In Demo Controller");
 		return "login";
 	}
 }
